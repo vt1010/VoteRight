1464a5b837a4f0e24c32f63be5682bedece1128f
Vasudevan Thanasekaran <Vasudevan.Thanasekaran@cornmarket.ie>
2026-01-12 16:17:49 +0000

    query

--- VoteRightWebApp/Utility/SqlQueries.cs
+++ VoteRightWebApp/Utility/SqlQueries.cs
@@ -37,20 +37,18 @@ public static class SqlQueries
     public static class Assemblies
     {                
         public const string DistinctByDistrictWithBoothCount = @"SELECT
-                                                                                                                                        v.constituency_details->>'assembly_constituency_name'   AS name,
-                                                                                                                                        v.constituency_details->>'assembly_constituency_number' AS number,
-                                                                                                                                        COUNT(DISTINCT v.constituency_details->>'part_number')  AS booth_count
-                                                                                                                                 FROM public.voters v
-                                                                                                                                 JOIN public.metadata m
-                                                                                                                                     ON m.part_no::text = v.constituency_details->>'part_number'
-                                                                                                                                 WHERE NULLIF(v.constituency_details->>'assembly_constituency_name', '') IS NOT NULL
-                                                                                                                                     AND NULLIF(v.constituency_details->>'assembly_constituency_number', '') IS NOT NULL
-                                                                                                                                     AND NULLIF(m.district, '') IS NOT NULL
-                                                                                                                                     AND m.district = @district
-                                                                                                                                 GROUP BY
-                                                                                                                                         v.constituency_details->>'assembly_constituency_name',
-                                                                                                                                         v.constituency_details->>'assembly_constituency_number'
-                                                                                                                                 ORDER BY CAST(v.constituency_details->>'assembly_constituency_number' AS INTEGER)";
+                                                                                                        m.constituency_details->>'assembly_constituency_name'   AS name,
+                                                                                                        m.constituency_details->>'assembly_constituency_number' AS number,
+                                                                                                        COUNT(DISTINCT m.constituency_details->>'part_number')  AS booth_count
+                                                                                                     FROM public.metadata m
+                                                                                                     WHERE NULLIF(m.constituency_details->>'assembly_constituency_name', '') IS NOT NULL
+                                                                                                         AND NULLIF(m.constituency_details->>'assembly_constituency_number', '') IS NOT NULL
+                                                                                                         AND NULLIF(m.district, '') IS NOT NULL
+                                                                                                         AND m.district = @district
+                                                                                                     GROUP BY
+                                                                                                             m.constituency_details->>'assembly_constituency_name',
+                                                                                                             m.constituency_details->>'assembly_constituency_number'
+                                                                                                     ORDER BY CAST(m.constituency_details->>'assembly_constituency_number' AS INTEGER)";
     }
 
     public static class Voters
